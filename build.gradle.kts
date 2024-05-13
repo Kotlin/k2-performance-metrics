e299e220580771a2feff1bf7a51adfd96246442b
Nataliya <nataliya.valtman@jetbrains.com>
2024-05-13 12:44:24 +0200

    Use kotlin 2.0.0-RC3 for benchmark_2_0 task (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ plugins {
 val downloadGradleProfileTask = DownloadGradleProfileTask.registerTask(project)
 
 val cleanGeneratedReports_2_0 = tasks.register<Delete>("cleanGeneratedReports2_0") {
-    delete(layout.projectDirectory.dir("reports/2.0.0-RC2"))
+    delete(layout.projectDirectory.dir("reports/2.0.0-RC3"))
 }
 
-val benchmark_2_0 = PerformanceTask.registerPerformanceTask(project, "benchmark_2_0", "2.0.0-RC2") {
+val benchmark_2_0 = PerformanceTask.registerPerformanceTask(project, "benchmark_2_0", "2.0.0-RC3") {
     dependsOn(cleanGeneratedReports_2_0)
     dependsOn(downloadGradleProfileTask)
 }
